709e9875324c328bd37d6cb915903f85b9ff24cb
Carlos Jiménez <charly.design.c@gmail.com>
2021-12-13 08:53:56 +0100

    planet map

--- client/src/Components/Planet-map/Planet/Planet.js
+++ client/src/Components/Planet-map/Planet/Planet.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Planet.css'
 
 export default function Planet(props) {
     return (
         <div className={props.className}>
             <p>{props.planetName}</p>
             <div>
-                <img src={props.planetImage} alt={props.planetImage}/>          
                 <img className={`light-${props.planetName}`} src={props.planetImage} alt={props.planetImage}/>
+                <img src={props.planetImage} alt={props.planetImage}/>          
             </div>          
         </div>
     )
