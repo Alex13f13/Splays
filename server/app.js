40f4a81231ce12c86eea4b52555c276dc66a190c
Carlos Jiménez <charly.design.c@gmail.com>
2021-12-16 11:21:03 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -1,30 +1,13 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv/config");
-
-// ℹ️ Connects to the database
-require("./db");
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
-
 const app = express();
+const path = require('path')
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("dotenv/config");
+require("./db");
 require("./config")(app);
 
-// 👇 Start handling routes here
-// Contrary to the views version, all routes are controlled from the routes/index.js
-
 app.use(express.static(path.join(__dirname, "public")));
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
-
-
+app.use("/api", require("./routes"));
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 module.exports = app;
