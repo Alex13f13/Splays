4ece38fd8716b9e1a99fe937af4c36ad1c957bbc
Carlos Jiménez <charly.design.c@gmail.com>
2021-12-16 19:14:28 +0100

    random game

--- client/src/Components/Challenge/Challenge.js
+++ client/src/Components/Challenge/Challenge.js
@@ -13,6 +13,7 @@ import Align from "./Challenges/Align/Align";
 import CantTouchThis from './Challenges/CantTouchThis/CantTouchThis'
 import './Challenge.css'
 
+
 export default function Challenge() {
 
     const params = useParams()
@@ -150,18 +151,167 @@ export default function Challenge() {
                 </div >
             )
 
-        default:
-            return (
-                <div>
-                    <div className="exit-btn-container">
-                        <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
-                    </div>
 
-                    <div className="btn-center-screen-container">
-                        <SaimonSays winRoute={`/planet-map/${params.id}/challenge/win`}></SaimonSays>
-                    </div>
-                </div>
-            )
+
+
+
+
+
+
+        default:
+
+            let allGames = ["pressFiveSec", "pressFiveTimes", "saimonSays", "speedButton", "asteroids", "intruderColor", "changeSameColor", "align", "cantTouchThis"]
+
+            let takeRandom = Math.round(Math.random() * allGames.length-1);
+
+            switch (allGames[takeRandom]) {
+                case 'pressFiveTimes':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <PressFiveTimes winRoute={`/planet-map/${params.id}/challenge/win`}></PressFiveTimes>
+                            </div>
+                        </div>
+                    )
+        
+        
+                case 'pressFiveSec':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <PressFiveSec winRoute={`/planet-map/${params.id}/challenge/win`}></PressFiveSec>
+                            </div>
+                        </div>
+                    )
+        
+                case 'theNight':
+                    return (
+                        <div>
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+                            <h1>The Night</h1>
+        
+                            <div className="btn-center-screen-container">
+                                <TheNight winRoute={`/planet-map/${params.id}/challenge/win`}></TheNight>
+                            </div>
+                        </div>
+                    )
+        
+                case 'saimonSays':
+                    return (
+                        <div>
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <SaimonSays winRoute={`/planet-map/${params.id}/challenge/win`}></SaimonSays>
+                            </div>
+                        </div>
+                    )
+        
+                case 'speedButton':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <SpeedButton winRoute={`/planet-map/${params.id}/challenge/win`}></SpeedButton>
+                            </div>
+                        </div>
+                    )
+        
+                case 'asteroids':
+                    return (
+                        <div>
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <Asteroids winRoute={`/planet-map/${params.id}/challenge/win`}></Asteroids>
+                            </div>
+                        </div>
+                    )
+        
+                case 'intruderColor':
+                    return (
+                        <div>
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <IntruderColor winRoute={`/planet-map/${params.id}/challenge/win`}></IntruderColor>
+                            </div>
+                        </div>
+                    )
+        
+                case 'changeSameColor':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <ChangeSameColor winRoute={`/planet-map/${params.id}/challenge/win`}></ChangeSameColor>
+                            </div>
+                        </div>
+                    )
+        
+                case 'align':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <Align winRoute={`/planet-map/${params.id}/challenge/win`}></Align>
+                            </div >
+                        </div >
+                    )
+        
+                case 'cantTouchThis':
+                    return (
+                        <div className="challenge-background">
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <CantTouchThis winRoute={`/planet-map/${params.id}/challenge/win`}></CantTouchThis>
+                            </div >
+                        </div >
+                    )
+        
+                default:
+                    return (
+                        <div>
+                            <div className="exit-btn-container">
+                                <Link className="challenge-exit-btn" to={'/planet-map'}>Exit</Link>
+                            </div>
+        
+                            <div className="btn-center-screen-container">
+                                <SaimonSays winRoute={`/planet-map/${params.id}/challenge/win`}></SaimonSays>
+                            </div>
+                        </div>
+                    )
+        
+            }
+            
 
     }
 }
